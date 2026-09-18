bbc620f61d8c765d1bc3fc717ab4f05c9fc6230b
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2026-09-17 20:52:09 -0400

    Updates to the Representational prototype
    I started making the representational prototype image in P5 and I decided to make a representation of an orange cat, with no braincells so it's easy to make them. The hardest part yet was figuring out how to place those tricky triangles for the ears.

--- Prototypes/Representational Prototype/js/script.js
+++ Prototypes/Representational Prototype/js/script.js
@@ -1,24 +1,57 @@
 /**
- * Title of Project
- * Author Name
+ * Representation of 
+ * Felipe Paiva
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This project contains representational things.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Setup creates a Canvas of a certain dimension.
 */
 function setup() {
+    createCanvas(500, 500);
 
+    
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * This draw function draws a cat.
 */
 function draw() {
+    // variables
+    let catColor = color(255, 204, 0);
+
+    // Creates a solid grey background
+    background(220);
+
+    // This draws the round shape of the cat's face with its signature color using the variable.
+    fill(catColor);
+    noStroke();
+    circle(250, 250, 400);
+
+    // This draws the two little pointy ears, first the left one then the right one inverting the x values.
+    fill(catColor);
+    noStroke();
+    triangle(100, 150, 250, 100, 100, 0);
+
+    fill(catColor);
+    noStroke();
+    triangle(400, 150, 250, 100, 400, 0);
+
+    // This draws the two eyes using circles
+
+    fill("white");
+    noStroke();
+    circle(175, 175, 50);
+    circle(325, 175, 50);
+
+    // This draws the pupils using ellipses
+    fill("black");
+    noStroke();
+    ellipse(175, 175, 10, 40);
+    ellipse(325, 175, 10, 40);
 
 }
