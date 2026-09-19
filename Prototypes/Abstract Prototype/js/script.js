0b0882a95507d36c97f4f693482db4d3e81b043b
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2026-09-18 23:08:45 -0400

    Added some more for loops that make some crazy shapes

--- Prototypes/Abstract Prototype/js/script.js
+++ Prototypes/Abstract Prototype/js/script.js
@@ -31,5 +31,36 @@ function draw() {
         // creates the rect using the i variable used in the for loop
         rect(i, i, i, i);
     };
-    
+
+    for (let i = 200; i <= 400; i++) {
+        // Makes the last rect a fill of black using an if statement targeting the last iteration
+        fill('orange');
+        if (i==400) {
+            fill('black');
+        }
+        // creates the rect using the i variable used in the for loop
+        rect(200+i, i, i, i);
+    };
+
+    for (let i = 1000; i >= 400; i--) {
+        // Makes the last rect a fill of black using an if statement targeting the last iteration
+        fill('brown');
+        if (i==400) {
+            fill('black');
+        }
+        // creates the rect using the i variable used in the for loop
+        rect(i, 0, i, 20);
+    };
+
+    for (a = 0; a <= 2; a++){
+        for (let i = 1000; i >= 400; i--) {
+            // Makes the last rect a fill of black using an if statement targeting the last iteration
+            fill('black');
+            if (i==400) {
+                fill('brown');
+            }
+            // creates the rect using the i variable used in the for loop
+            rect(i, 20, i, 40);
+        };
+    }
 }
