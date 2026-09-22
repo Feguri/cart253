f2402d98c6c9c56c27591d41084cec427b542764
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2026-09-22 15:07:21 -0400

    Updated and finished the last (really weird) prototype
    It uses a simple loop function that detects whether the mouse moved and which position it has moved, and draws a rect with a random color in the screen.
    
    I also made a separate epilepsy warning using HTML and a bit of javaScript. Shoutout to the other Felipe for sending me a good epilepsy text to put as a warning.

--- Prototypes/Really Weird Prototype/js/script.js
+++ Prototypes/Really Weird Prototype/js/script.js
@@ -8,20 +8,38 @@
 "use strict";
 
 /**
- * Setup creates a Canvas of a certain dimension (500x500)
+ * Setup creates a Canvas of a certain dimension (1000x500)
 */
+
+// a variable to see if the continue button has been clicked, to not run the code if it's false
+let hasClicked = false;
+
+// Sets up the canvas
 function setup() {
-    createCanvas(500, 500);
+    // creates a canvas
+    createCanvas(1000, 500);
+    // creates a variable for the continue button
+    let continueBtn = select('#continue');
+  
+    // When clicked, run an anonymous arrow function
+    continueBtn.mousePressed(() => {
+        hasClicked = true; 
+    });
 }
 
 
 /**
- * This draw function draws a cat.
+ * This draw function is very simple, it creates a window even listener loop for the mouse's x and y positions, then draws a rect with a random color every time the position changes, using the sizes of the rect as the x and y click events.
 */
 function draw() {
-    window.addEventListener('mousemove', (event) => {
-        const x = event.clientX;
-        const y = event.clientY; 
-        console.log(`Mouse Position -> X: ${x}, Y: ${y}`);
-});
+    fill('Black');
+    if (hasClicked) {
+        window.addEventListener('mousemove', (event) => {
+            var myColor = color(random(255), random(255), random(255));
+            const x = event.clientX;
+            const y = event.clientY; 
+            fill(myColor);
+            rect(0, 0, x, y);
+    });
+    }
 }
