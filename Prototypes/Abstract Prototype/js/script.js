bf7247cd611d6fb625c30f42287570e246d0cfca
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2026-09-22 11:31:00 -0400

    Updated the draw function in the abstract prototype and finished the artwork
    Included many new descriptions for the project and finished the shapes with a new for loop

--- Prototypes/Abstract Prototype/js/script.js
+++ Prototypes/Abstract Prototype/js/script.js
@@ -18,8 +18,12 @@ function setup() {
  * This draw function draws the abstraction
 */
 function draw() {
-    background(230);
-    fill("darkgreen");
+
+    // sets the background of the scene to a dark grey
+    background(40);
+    // Sets the background color of the fill of the first shape to purple
+    fill("purple");
+    // Removes all strokes
     noStroke();
 
     // Some simple for loop that allows the creation of an interesting shape
@@ -32,9 +36,11 @@ function draw() {
         rect(i, i, i, i);
     };
 
+    // Another for loop for a similar interesting shape, but at a different location.
     for (let i = 200; i <= 400; i++) {
-        // Makes the last rect a fill of black using an if statement targeting the last iteration
+        // changes the color of all rects to orange
         fill('orange');
+        // Makes the last rect a fill of black using an if statement targeting the last iteration
         if (i==400) {
             fill('black');
         }
@@ -42,25 +48,23 @@ function draw() {
         rect(200+i, i, i, i);
     };
 
-    for (let i = 1000; i >= 400; i--) {
-        // Makes the last rect a fill of black using an if statement targeting the last iteration
-        fill('brown');
-        if (i==400) {
-            fill('black');
-        }
-        // creates the rect using the i variable used in the for loop
-        rect(i, 0, i, 20);
-    };
-
-    for (a = 0; a <= 2; a++){
+    // Sets a position variable to be used in the "Stairs" shape
+    let Position = 0;
+    // This for loop creates a shape in the shape of staris
+    for (let i = 0; i <= 30; i++) {
         for (let i = 1000; i >= 400; i--) {
+            // Makes the shape's color brown
+            fill('brown');
             // Makes the last rect a fill of black using an if statement targeting the last iteration
-            fill('black');
             if (i==400) {
-                fill('brown');
+                fill('black');
             }
-            // creates the rect using the i variable used in the for loop
-            rect(i, 20, i, 40);
+            // creates the rect using the i variable used in the for loop and the position variable
+            rect(i + Position, Position, i, 20);
+            
         };
+        // Adds to the position variable for greater variation and creating the "stairs" look. Changing this value
+        // Will make the stairs shape change
+        Position += 25;
     }
 }
